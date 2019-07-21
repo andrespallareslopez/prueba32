836505dea13b053b1ad7fc4ce65cf9ce6d356380
andrespallareslopez <andres_pallares@hotmail.com>
2019-07-21 20:49:22 +0200

    Commit 02

---
+++ src/componentsImports/UXDataList.js
@@ -0,0 +1,136 @@
+/// <reference path="../shared/js/main.d.ts" />
+
+import {util} from './util01.js';
+import {components} from './components01.js';
+
+var controls;
+
+;(function(ns,components,$){
+    "use strict";
+    ns.UXDataList=(function(){
+       return function(options){
+                      
+        (function(options){
+            options.id=options.id||undefined
+            options.containerComponent=options.containerComponent||".panel-datalist"
+            options.data=options.data||undefined
+            options.nameblock=options.nameblock||'.group-block'
+            options.namefield=options.namefield||'#combotxt'
+            options.namedatalist=options.namedatalist||'.datalist'
+            options.namebutton=options.namebutton||'#search'
+            options.namebuttonclose=options.namebuttonclose||'#btn-slide-close01'
+            options.inputText=options.inputText||'#combotxt' 
+            options.onButtonClick=options.onButtonClick||undefined
+            options.onClose=options.onClose||undefined
+        })(options)
+        //renombramos el datalist
+        var id=options.id;
+        var namedatalist=id+"datalist";
+        $("#"+id+" "+options.namedatalist).get(0).id=namedatalist;
+        //******************************** */
+        options.container="#"+namedatalist+" "+options.containerComponent;
+        //********************************* */
+        this.$inputText=undefined
+
+        this.init(options)
+        //Si no estubiera definida la propiedad textTemplate entonces
+        //no va a iniciar el initevent desde components01.js, por tanto
+        //tenemos que lanzarlo nosotros manualmente.
+        if (!this.options.textTemplate){
+           
+            this.initevent()
+        }
+       }
+    })();
+    ns.UXDataList.prototype=new components.container();
+    
+    /*
+    ns.UXDataList.prototype.init=function(){
+    }
+    */
+    ns.UXDataList.prototype.initevent=function(){
+        //console.log("estoy dentro de initevent de uxdatalist"+this.options.id)
+        var self=this
+        var id=self.options.id;
+        var namedatalist=id+"datalist";
+        //console.log("UXDataList "+id)
+        //console.dir( $("#"+id+" "+".datalist").get(0).id)
+        $("#"+id+" "+self.options.namedatalist).get(0).id=namedatalist;
+        //console.dir($("#"+namedatalist))
+        $("#"+id+" "+self.options.namebutton).on("click", function (e) {
+            
+            e.preventDefault();
+            //console.log("estoy dentro")
+            //console.dir(e)
+            $("#"+namedatalist).toggleClass("tether-open");
+            telist.position();
+            
+             
+            if (self.options.onButtonClick  && $("#"+namedatalist).hasClass("tether-open")){
+                console.log("estoy dentro de onButtonClick")
+                self.options.onButtonClick(self.options)
+            }else if ((self.options.fncreate ) && $("#"+namedatalist).hasClass("tether-open")){
+                console.log("estoy dentro de fncreate")
+                self.options.fncreate()
+            }
+        });
+        $("#"+namedatalist+" "+self.options.namebuttonclose).on("click",function(e){
+           e.preventDefault()
+           
+           //console.log("estoy dentro")
+           $("#"+namedatalist).toggleClass("tether-open");
+           telist.position();
+           
+           if (self.options.onClose){
+               self.options.onClose()
+           }
+           
+        })
+        
+        $("#"+id+" "+self.options.nameblock).on("click",function(e){
+            //console.log("estoy dentro")
+            //console.dir(e)
+            e.preventDefault()
+            if ($("#"+namedatalist).hasClass("tether-open") && (e.target.className!='fa fa-sort-desc' && e.target.id!=self.options.namebutton.replace('#',''))){
+               //cerramos el panel del combo si pinchamos en cualquier lugar del control
+               $("#"+namedatalist).toggleClass("tether-open");
+               telist.position();
+               if (self.options.onClose){
+                self.options.onClose()
+               }
+            }
+        })
+        
+        var telist = new Tether({
+            element: "#"+namedatalist,
+            target: "#"+id+" "+self.options.namebutton,
+            attachment: "top right",
+            targetAttachment: 'bottom left',
+            constraints: [
+                {
+                    to: 'scrollParent',
+                    pin: true
+                }]
+        });
+        telist.position();
+        
+        self.$telist=telist;
+        self.$inputText=$("#"+id+" "+self.options.inputText);
+        //console.dir(self.$inputText)
+    }
+    
+    ns.UXDataList.prototype.toggle=function(){
+       var self=this
+       var id=self.options.id;
+       var namedatalist=id+"datalist";
+       $("#"+namedatalist).toggleClass("tether-open");
+       self.$telist.position();
+
+    }
+
+})(controls=controls||{},components,jQuery)
+
+export {controls}
+
+util.addNameSpace(window.controls,controls);
+
